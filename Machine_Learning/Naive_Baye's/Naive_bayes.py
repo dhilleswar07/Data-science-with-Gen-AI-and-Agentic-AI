d7a04d05cb2875c9cee52838635ade5cd04e8246
DHILLESWAR JOGI <dhilleswarjogivijay@gmail.com>
2026-07-22 17:38:05 +0530

    Add files via upload

---
+++ Machine_Learning/Naive_Baye's/Naive_bayes.py
@@ -0,0 +1,71 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv(r"E:\DATASCIENCE WITH GEN AI & AGENTIC AI\logit classification.csv")
+
+x = dataset.iloc[:, [2,3]].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+x_train, x_test,y_train,y_test = train_test_split(x,y,test_size=0.20,random_state=0)
+
+
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc=  StandardScaler()
+x_train= sc.fit_transform(x_train)
+x_test= sc.transform(x_test)
+
+'''
+from sklearn.preprocessing import Normalizer
+sc=  Normalizer()
+x_train= sc.fit_transform(x_train)
+x_test= sc.transform(x_test)
+
+'''
+
+'''
+# Training the naive bayes model on the training set
+from sklearn.naive_bayes import MultinomialNB
+classifier = MultinomialNB()
+classifier.fit(x_train,y_train)
+
+# Training the naive bayes model on the training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(x_train,y_train)
+
+'''
+
+# Training the naive bayes model on the training set
+from sklearn.naive_bayes import BernoulliNB
+classifier = BernoulliNB()
+classifier.fit(x_train,y_train)
+
+
+y_pred = classifier.predict(x_test)
+
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print("Confusion_matrix:\n",cm)
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+
+from sklearn.metrics import classification_report
+cr = classification_report(y_test, y_pred)
+print("classifier report:\n",cr)
+
+bias = classifier.score(x_train,y_train)
+print("bias:",bias)
+
+variance= classifier.score(x_test, y_test)
+print("variance:",variance)
+
+
+
+
+
